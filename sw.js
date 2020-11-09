eb9c076ff74c81ab7dd0248d752b8598bf48e3b2
Jack <a2data@vip.qq.com>
2020-11-09 22:14:50 +0800

    Site updated: 2020-11-09 22:14:46

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","ec008d3a01a39f0f1b0787c88654a3a2"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
